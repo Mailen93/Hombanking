e30fcbb138e57a4b15d87c1d83610a361650c783
Mailen93 <m.labarrere2@gmail.com>
2023-03-09 22:50:31 -0300

    alert

--- homebanking-ultimaversion/homebanking/src/main/resources/static/Assets/javascript/index.js
+++ homebanking-ultimaversion/homebanking/src/main/resources/static/Assets/javascript/index.js
@@ -30,16 +30,41 @@ createApp({
         },
         register() {
             axios.post('/api/clients', `first=${this.firstName}&lastName=${this.lastName}&email=${this.email}&password=${this.password}`, {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                }
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                })
+            .then(()=>{
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: "top-end",
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                      toast.addEventListener("mouseenter", Swal.stopTimer);
+                      toast.addEventListener("mouseleave", Swal.resumeTimer);
+                    },
+                  });
+      
+                  Toast.fire({
+                    icon: "success",
+                    title: "Signed in successfully",
             })
             .then(response => {
                this.login();
             })
+            })
             .catch(error => {
                 this.error = error.response.data.message;
-            });
+                Swal.fire({
+                    title: 'Missing data',
+                    showClass: {
+                      popup: 'animate__animated animate__fadeInDown'
+                    },
+                    hideClass: {
+                      popup: 'animate__animated animate__fadeOutUp'
+                    }
+                  })
+            })
     },
     createCard(){
         axios.post('/api/clients/current/cards', `type=${this.cardType}&color=${this.cardColor}`,{
